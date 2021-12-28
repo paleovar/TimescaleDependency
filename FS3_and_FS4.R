d72a8112d77693640a0f9f798378f1558091fe73
Beatrice Ellerhoff <beatrice.ellerhoff@iup.uni-heidelberg.de>
2021-12-28 13:18:37 +0100

    FS4 added

---
+++ FS3_and_FS4.R
@@ -0,0 +1,38 @@
+#---------------------#
+#FS3
+source("helpers/init.R")
+source("helpers/functions.R")
+tbb <- readRDS("data/pages_meta_scaling.Rds")
+
+#pages.prxlist <- readRDS(paste0(pages.dir, "/pages.prxlist_", min.res, "_", min.range, "_", max.hiat, ".Rds"))
+pages.prxlist <- readRDS("data/proxylist.Rds") %>% inner_join(., tbb,  by="Name") 
+
+pages.prxlist$data2 <- lapply(pages.prxlist$data, function(x) tibble(time=index(x) / 1000, val=coredata(x) - mean(na.approx(coredata(x)))))
+  
+dat <- pages.prxlist %>% unnest(data2)
+
+ggplot(dat, aes(x=time, y=val)) + 
+    geom_line(size=0.1) + xlab("time (kyr CE)") + ylab(TeX("Temperature anomaly (K)")) + geom_line(size=0.1) +
+    scale_x_continuous( labels = scales::number_format(accuracy = 0.1), expand=c(0.1,0.1)) +
+    scale_y_continuous(sec.axis = dup_axis(name = NULL, labels = NULL)) +
+    theme_td() + 
+    theme(legend.position = "none") + facet_wrap(~ID, scales = "free_x") 
+
+#---------------------#
+#FS4
+tmp_spec <- readRDS("data/proxy_spectra.Rds") 
+tmp_spec <- tmp_spec %>% inner_join(., tbb,by="Name")
+
+yrs.period <- rev(c(0.0001, 0.001, 0.01,  0.1, 1, 10, 100, 1000, 10000, 100000, 1000000))
+yrs.labels <- rev(c(TeX('$10^{-4}$'),TeX('$10^{-3}$'), TeX('$10^{-2}$'), TeX('$10^{-1}$'), TeX('$10^{0}$'), TeX('$10^{1}$'), TeX('$10^{2}$'), TeX('$10^{3}$'), TeX('$10^{4}$'), TeX('$10^{5}$'), TeX('$10^{6}$')))
+
+ggplot(tmp_spec %>% unnest(data), aes(x=freq, y=spec)) + 
+    facet_wrap(~ID) +
+    geom_line(aes(x=1/freq, y = spec),color="black", size=0.1) +
+    scale_y_log10(labels = trans_format("log10", math_format(10^.x)), name=TeX('PSD $S(\\tau)\\, (K^2 yr)$ '), expand=c(0.2, 0.2), 
+                  sec.axis = dup_axis(name = NULL, labels = NULL))  + 
+    scale_x_continuous(trans=reverselog_trans(10), breaks = yrs.period, labels = yrs.labels,   
+                       expand=c(0.05, 0.05), limits=c(1e3, 3), name=TeX('period $\\tau\\,(yr)$')) + 
+    theme(legend.position="none")  +
+    annotate(geom="rect", xmin = c(10), ymin = c(0), xmax = c(200), ymax = c(Inf), fill="#f5793a", alpha=0.3)+ 
+    theme_td()
