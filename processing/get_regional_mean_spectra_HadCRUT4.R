16b4cc3b79dc36cd4fe022b63276f4594996c1a0
Beatrice Ellerhoff <beatrice.ellerhoff@iup.uni-heidelberg.de>
2022-01-05 16:04:37 +0100

    added HadCRUT4 spectra

--- processing/get_regional_mean_spectra_HadCRUT4.R
+++ processing/get_regional_mean_spectra_HadCRUT4.R
@@ -1,5 +1,5 @@
-
-source("helpers/init.R")
+source("helpers/init.R")#
+source("helpers/functions.R")
 library(ncdf4)
 
 #define parameters
@@ -14,8 +14,8 @@ dat <- list()
 dat$lat <- ncdf4::ncvar_get(ncf,varid = "latitude")
 dat$lon <- ncdf4::ncvar_get(ncf,varid = "longitude")
 dat$tanom <- ncdf4::ncvar_get(ncf,varid = "temperature_anomaly")
-dat$time <- ncdf4::ncvar_get(ncf,varid = "time")
-dat$time <- as.Date(dat$time,origin="1850-1-1")
+gregorian_year <- 365.2425
+dat$time <- ncdf4::ncvar_get(ncf,varid = "time")/gregorian_year
 dimnames(dat$tanom)<-list(longitude=dat$lon,latitude=dat$lat,time=dat$time)
 
 #get coordinates
@@ -28,7 +28,8 @@ for(i in 1:length(dat$lon)){
 
 #extract long series
 coverage <- function(min.len, dat){
-  result <- list()
+  result1 <- list()
+  result2 <- list()
   for(i in 1:length(coords)){
       loc <- list()
       loc$lon <- coords[[i]][1]
@@ -44,17 +45,29 @@ coverage <- function(min.len, dat){
       if(all(is.na(target.ano))==FALSE){
       df <- na.approx(coredata(dat$tanom[lon.i,lat.i,]), x = index(dat$tanom[lon.i,lat.i,]), na.rm = FALSE, maxgap=2)
       y1 <- length(na.contiguous(df))
+      df <- na.contiguous(df)
 
-      if(y1 >= min.len)
-        result[[length(result)+1]] <- c(coords[[i]]) #, y1
+      if(y1 >= min.len){
+        result1[[length(result1)+1]] <- c(coords[[i]]) #, y1
+        result2[[length(result2)+1]] <- MakeEquidistant(t.x=seq(1:length(df))/12, t.y= df-mean(df), dt = 1/12)
+        }
       }
       }
-  return(result)
+  return(list("coords_list"=result1, "temp_list"=result2))
 }
 
+seq(1:length(1:10))/12
+
 min.length <- 12*hadcrut.len
-coords_list <- coverage(min.length, dat)
+result <- coverage(min.length, dat)
+length(result$temp_list)
 
 if(save){
     saveRDS(coords_list, paste0("processing/raw_data/coords/coords_HadCRUT4_len", hadcrut.len, ".Rds"))
 }
+
+lats <- unlist(lapply(result$coords_list, function(x) x[2]))
+w.lats <- cos(lats*pi/180)/sum(cos(lats*pi/180))
+specs <- lapply(result$temp_list, function(x) SpecMTM(x, k=3, nw=2, detrend=T))
+RMST <- list()
+RMST$HadCRUT4 <- MeanSpec(specs, weights=w.lats)
