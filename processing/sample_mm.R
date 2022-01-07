5da79c15bc496f32b4f2b1b7be7d8297f39fd84b
Beatrice Ellerhoff <beatrice.ellerhoff@iup.uni-heidelberg.de>
2022-01-07 10:44:48 +0100

    option to choose between enso or not

--- processing/sample_mm.R
+++ processing/sample_mm.R
@@ -2,35 +2,45 @@ if(!exists("samples")){
   samples <- list()
 }
 
-speclist_smoothed_tbb <- readRDS("data/global_mean_spectra.Rds")
+for(wENSO in c(T,F)){
+  speclist_smoothed_tbb <- readRDS("data/global_mean_spectra.Rds")
 
-obs <- c(signal_tbb %>% filter(type=="obs"))$signal
+  obs <- c(signal_tbb %>% filter(type=="obs"))$signal
 
-w <- rep(1, length(speclist_smoothed_tbb$signal))
-w[which(speclist_smoothed_tbb$signal %in% names_echam)] <- echam_weights
-w[which(speclist_smoothed_tbb$signal %in% control_runs)] <- control_runs_weights
-w[which(speclist_smoothed_tbb$signal %in% obs)] <- 0
+  w <- rep(1, length(speclist_smoothed_tbb$signal))
+  w[which(speclist_smoothed_tbb$signal %in% control_runs)] <- control_runs_weights
+  w[which(speclist_smoothed_tbb$signal %in% names_echam)] <- echam_weights
+  w[which(speclist_smoothed_tbb$signal %in% obs)] <- 0
 
-speclist_smoothed <- tibble_to_list(speclist_smoothed_tbb, name.data="data")
-names(speclist_smoothed) <- speclist_smoothed_tbb$signal
+  if(wENSO==T){
+    w[which(speclist_smoothed_tbb$signal == "MPI-M")] <- 1
+  } 
 
-speclist_smoothed[which(w==0)] <- NULL
-names(speclist_smoothed)
-speclist_smoothed_tbb$mean <- NULL
+  speclist_smoothed <- tibble_to_list(speclist_smoothed_tbb, name.data="data")
+  names(speclist_smoothed) <- speclist_smoothed_tbb$signal
 
-w <- rep(1, length(names(speclist_smoothed)))
+  speclist_smoothed[which(w==0)] <- NULL
+  names(speclist_smoothed)
+  speclist_smoothed$mean <- NULL
 
-speclist <- lapply(speclist_smoothed, function(x) cut(x, from=5000, 12/9, index=FALSE))
-l <- list()
-len <- length(w[w!=0])
-for(i in 1:1000){
-  w <- sample(0:1000, len, replace = TRUE)/1000
-  l[[i]] <- MeanSpec(speclist, weights=w)$spec 
-  #l$mean$dof <- NULL
-  l[[i]]$lim.1 <- NULL
-  l[[i]]$lim.2 <- NULL
+  w <- rep(1, length(names(speclist_smoothed)))
+
+  speclist <- lapply(speclist_smoothed, function(x) cut(x, from=5000, 12/9, index=FALSE))
+  l <- list()
+  len <- length(w[w!=0])
+  for(i in 1:1000){
+    w <- sample(0:1000, len, replace = TRUE)/1000
+    l[[i]] <- MeanSpec(speclist, weights=w)$spec 
+    #l$mean$dof <- NULL
+    l[[i]]$lim.1 <- NULL
+    l[[i]]$lim.2 <- NULL
+  }
+
+  if(wENSO==T){
+    samples$models_wENSO <- l
+  } else {
+    samples$models_woENSO <- l
+  }
+  rm(l)
+  gc()
 }
-
-samples$models <- l
-rm(l)
-gc()
